c7730aaebcacbdbf64a1574bec20ee17c53593a6
Eric Lopez <hello@ericlopez.me>
2020-01-08 19:28:46 +0100

    evaluate whether the given request matches an expectation

--- expectation/expectation.go
+++ expectation/expectation.go
@@ -1,5 +1,7 @@
 package expectation
 
+import "strings"
+
 type expectation struct {
 	endpoint Endpoint
 	response Response
@@ -13,5 +15,31 @@ func responseForEndpoint(e Endpoint, r Response) expectation {
 }
 
 func (e expectation) matches(endpoint Endpoint) bool {
+	if e.endpoint.method != endpoint.method {
+		return false
+	}
+
+	if e.endpoint.path != "" && e.endpoint.path != endpoint.path {
+		return false
+	}
+
+	for _, expectedHeader := range e.endpoint.headers {
+		var headerMatches bool
+		expectedHeaderStr := strings.Join(expectedHeader, ",")
+
+		for _, givenHeader := range endpoint.headers {
+			givenHeaderStr := strings.Join(givenHeader, ",")
+
+			if expectedHeaderStr == givenHeaderStr {
+				headerMatches = true
+				break
+			}
+		}
+
+		if !headerMatches {
+			return false
+		}
+	}
+
 	return true
 }
